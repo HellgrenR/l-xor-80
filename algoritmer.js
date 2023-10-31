dd1349e3640bba84e3cd753306a1da5951bae695
HellgrenR <rallenolltva@gmail.com>
2023-10-31 01:17:40 +0100

    initial commit
    committar allt

---
+++ algoritmer.js
@@ -0,0 +1,74 @@
+
+// 1. Skriv en funktion som skriver ut kvadraten på ett tal som läses in. Kvadrat är talet multiplicerat med sig själv.
+
+// function squared(x) {
+//   return x * x
+// }
+
+// console.log(squared(4))
+
+
+
+// 2. En försäljare har delvis prestationslön. Han får 8000 kr per månad i grundlön och 9% av försäljningssumman.
+// Skriv en funktion som beräknar lönesumman under en period. Försäljningssumman ska läsas in.
+
+// function salary(months, sellSum) {
+//   const basePay = 8000
+//   return basePay * months + sellSum * 0.09
+// }
+
+// console.log(salary(2, 100))
+
+
+
+// 3. Skriv en funktion som läser in antal timmar, beräknar och skriver ut hur mycket det blir omvandlat till minuter resp sekunder.
+
+// function hourDecoder(hours) {
+//   return hours * 60 + "min, " + hours * 3600 + "sec"
+// }
+
+// console.log(hourDecoder(1))
+
+
+
+// 4. Skriv en funktion som läser in tre tal och beräknar och skriver ut summan samt medelvärdet av de tre talen.
+
+// function sumAverage(x, y, z) {
+//   let sum = x + y + z
+//   let average = sum / 3
+//   return "Summan = " + sum + "\nMedelvärdet = " + average
+// }
+
+// console.log(sumAverage(2, 2, 2))
+
+
+
+// 5. Läs in ett heltal motsvarande svenska kronor och skriv ut motsvarande värde i pund respektive dollar.
+// Antag att kursen är: 1 dollar = 6 kr, 1 pund = 10 kr.
+
+// function sekToPoundDollar(sek) {
+//   let pound = sek / 10
+//   let dollar = sek / 6
+//   return pound + " £\n" + dollar + " $"
+// }
+
+// console.log(sekToPoundDollar(6))
+
+
+
+// 6. Skriv en funktion som beräknar funktionen ax3 + 7 och skriver ut resultatet. Variablerna a och x ska läsas in.
+
+function algebra(a, x) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
